ada2712322f7bf4a7b935c60f758413265673e02
Oscar Vesga <oscar.vesga@ilumno.com>
2018-05-01 12:21:29 -0500

    fix print error in HitosIV.py

--- HitosIV.py
+++ HitosIV.py
@@ -3,12 +3,12 @@ import json
 class HitosIV:
 	"""Una clase para los hitos del curso arquitectura con docker """
 
-	def __init__(self, arg):
+	def __init__(self):
 		try:  # De https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file
 			with open('hitos.json') as data_file:
 				self.hitos = json.load(data_file)
 		except IOError as e:
-			print "Error {0} leyendo hitos.json: {1}", format(e.errno,e.strerror)
+			print("Error %d leyendo hitos.json: %s", e.errno,e.strerror)
 
 	def todos_hitos(self):
 		return self.hitos
